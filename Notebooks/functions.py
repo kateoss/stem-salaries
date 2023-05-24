619a6ae33bcb94b6380fc8b988b9fcc65f68d584
Kate Osypova <kateosypova@gmail.com>
2023-05-24 17:11:59 +0200

    transforming data, training models, working on slides

--- Notebooks/functions.py
+++ Notebooks/functions.py
@@ -15,3 +15,37 @@ def replace_with_other(df, col_name, threshold):
     low_values_count = values_count[values_count < threshold].index
     df.loc[df[col_name].isin(low_values_count), col_name] = "other"
     return df
+
+def error_metrics_calculations(y_true, y_pred):
+
+        """
+    This function given a model predicted and real values calculates error metrics.
+    Args:
+       y_true: actual y values in a test set
+       y_pred: predicted by model y values
+    Returns:
+       pandas.DataFrame: the dataframe with error metrics name and values
+    """
+        
+    error_metrics = {"MAE": mean_absolute_error(y_true, y_pred), 'MSE': mean_squared_error(y_true, y_pred), 
+                     'RMSE': np.sqrt(mean_squared_error(y_true, y_pred)), 'MAPE': np.mean(np.abs((y_true - y_pred) / y_true)) * 100,
+                     'R2': r2_score(y_true, y_pred)}
+    df = pd.DataFrame(list(error_metrics.items()), columns=['Error_metric', 'Value'])
+    return df
+
+def error_metrics_calculations(y_true, y_pred):
+
+    """
+    This function given a model predicted and real values calculates error metrics.
+    Args:
+       y_true: actual y values in a test set
+       y_pred: predicted by model y values
+    Returns:
+       pandas.DataFrame: the dataframe with error metrics name and values
+    """
+        
+    error_metrics = {"MAE": mean_absolute_error(y_true, y_pred), 'MSE': mean_squared_error(y_true, y_pred), 
+                     'RMSE': np.sqrt(mean_squared_error(y_true, y_pred)), 'MAPE': np.mean(np.abs((y_true - y_pred) / y_true)) * 100,
+                     'R2': r2_score(y_true, y_pred)}
+    df = pd.DataFrame(list(error_metrics.items()), columns=['Error_metric', 'Value'])
+    return df
